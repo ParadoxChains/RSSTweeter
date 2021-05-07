74466dee7b2f138eb67c00fb858b51840da23fcb
ParadoxChains <sitt.evan@gmail.com>
2021-05-07 17:45:44 -0500

    Create HTML Stripper Function

---
+++ HTMLContentHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace RSSTweeter
+{
+    class HTMLContentHandler
+    {
+        public static string StripHtml(string html)
+        {
+            html = html.Replace(">", "> ");
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            string text = HttpUtility.HtmlDecode(doc.DocumentNode.InnerText);
+            return Regex.Replace(text, @"\s+", " ").Trim();
+        }
+    }
+}
