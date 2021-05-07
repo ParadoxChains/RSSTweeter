e0665f522a51411ef16ac6b674b450083291ad50
ParadoxChains <sitt.evan@gmail.com>
2021-05-07 12:30:05 -0500

    Change number of fetched items from RSS

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace RSSTweeter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string feedURI = "https://www.pftq.com/forums/?type=rss;action=.xml;boards=72;limit=5";
+            string feedURI = "https://www.pftq.com/forums/?type=rss;action=.xml;boards=72;limit=10";
             RSSFeed newFeed = RSSFetch.readFeed(feedURI);
             Console.WriteLine(newFeed);
             
