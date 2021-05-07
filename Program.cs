caeead223e2f1da9516b1c99b15917e01ee3c84e
ParadoxChains <sitt.evan@gmail.com>
2021-05-07 17:46:00 -0500

    Insert testing function for HTML Stripper

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace RSSTweeter
             string feedURI = "https://www.pftq.com/forums/?type=rss;action=.xml;boards=72;limit=10";
             RSSFeed newFeed = RSSFetch.readFeed(feedURI);
             Console.WriteLine(newFeed);
-            
+            Console.WriteLine(HTMLContentHandler.StripHtml(newFeed.items[0].description));
         }
 
     }
