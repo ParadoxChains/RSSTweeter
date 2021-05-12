70845e6109009d725cdefb9801dec38be7967a1d
ParadoxChains <sitt.evan@gmail.com>
2021-05-11 21:58:52 -0500

    Configuration Loader class. Loads config.txt in constructor.

---
+++ ConfigurationLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RSSTweeter
+{
+    class ConfigurationLoader
+    {
+        private string[] loadedFile;
+
+        public ConfigurationLoader()
+        {
+            loadedFile = System.IO.File.ReadAllLines("config.txt");
+            foreach(string x in loadedFile)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public string getAPIKey()
+        {
+            return loadedFile[1];
+        }
+
+        public string getAPISecretKey()
+        {
+            return loadedFile[3];
+        }
+
+        public string getBearerToken()
+        {
+            return loadedFile[5];
+        }
+
+        public string getAccessToken()
+        {
+            return loadedFile[7];
+        }
+
+        public string getAccessTokenSecret()
+        {
+            return loadedFile[9];
+        }
+    }
+}
