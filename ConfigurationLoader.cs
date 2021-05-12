b9f0640157d7540c7b41bc1b70fddeeb089ad9bc
ParadoxChains <sitt.evan@gmail.com>
2021-05-11 22:03:19 -0500

    Adding in code documentation of expected file format

--- ConfigurationLoader.cs
+++ ConfigurationLoader.cs
@@ -9,7 +9,24 @@ namespace RSSTweeter
         public ConfigurationLoader()
         {
             loadedFile = System.IO.File.ReadAllLines("config.txt");
-            foreach(string x in loadedFile)
+            /*
+             * Configuration file must be named "config.txt" and placed in
+             * same directory as the .exe file.
+             * The expected format of the file is as follows:
+             * -----------------------------------------------------------
+             * API Key
+             * $TWITTER_API_KEY$
+             * API Secret Key
+             * $TWITTER_API_SECRET_KEY$
+             * Bearer Token
+             * $TWITTER_BEARER_TOKEN$
+             * Access Token
+             * $TWITTER_ACCESS_TOKEN$
+             * Access Token Secret
+             * $TWITTER_ACCESS_TOKEN_SECRET$
+             * -----------------------------------------------------------
+             */
+            foreach (string x in loadedFile)
             {
                 Console.WriteLine(x);
             }
